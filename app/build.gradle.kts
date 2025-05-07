c38c7cff830222c77c50bd427f1fcc454324ee2b
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-05-07 13:16:08 +0200

    stop f**king with the splash screen -_-

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.material3)
-    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
