022337705cd095510eb3b3f8e6fb9bf3c4577706
Aleksandr Naumenko <saniok017@gmail.com>
2025-05-10 14:24:03 +0200

    feature: Change class filter

--- app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/SpellSearchScreen.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/SpellSearchScreen.kt
@@ -2,19 +2,23 @@ package com.github.arhor.spellbindr.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -42,7 +46,7 @@ fun SpellSearchScreen(
     val spellListViewState by spellListViewModel.state.collectAsState()
     val spellSearchViewState by spellSearchViewModel.state.collectAsState()
 
-    var expandedClass by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
     var showAddDialog by remember { mutableStateOf(false) }
     var selectedSpellName by remember { mutableStateOf<String?>(null) }
 
@@ -51,39 +55,42 @@ fun SpellSearchScreen(
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        ExposedDropdownMenuBox(
-            expanded = expandedClass,
-            onExpandedChange = { expandedClass = !expandedClass }
+        Row (
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            OutlinedTextField(
-                value = spellSearchViewState.selectedClass?.toString() ?: "All Classes",
-                onValueChange = {},
-                readOnly = true,
-                label = { Text("Filter by Class") },
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedClass) },
-                modifier = Modifier
-                    .menuAnchor(MenuAnchorType.PrimaryNotEditable)
-                    .fillMaxWidth(),
+            Text(
+                text = "Spell Book",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge,
             )
-            ExposedDropdownMenu(
-                expanded = expandedClass,
-                onDismissRequest = { expandedClass = false }
+            Spacer(Modifier.weight(1f))
+            Box(
+                modifier = Modifier.padding(end = 10.dp)
             ) {
-                DropdownMenuItem(
-                    text = { Text("All Classes") },
-                    onClick = {
-                        spellSearchViewModel.onClassFilterChanged(null)
-                        expandedClass = false
-                    }
-                )
-                SpellcastingClass.entries.forEach { spellClass ->
+                IconButton(onClick = { expanded = !expanded }) {
+                    Icon(Icons.Default.MoreVert, contentDescription = "Filter by class")
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
                     DropdownMenuItem(
-                        text = { Text(spellClass.toString()) },
+                        text = { Text("All Classes") },
                         onClick = {
-                            spellSearchViewModel.onClassFilterChanged(spellClass)
-                            expandedClass = false
+                            spellSearchViewModel.onClassFilterChanged(null)
+                            expanded = false
                         }
                     )
+                    SpellcastingClass.entries.forEach { spellClass ->
+                        DropdownMenuItem(
+                            text = { Text(spellClass.toString()) },
+                            leadingIcon = { Icon(Icons.Outlined.Info, contentDescription = null) }, // TODO change to class Icon
+                            onClick = {
+                                spellSearchViewModel.onClassFilterChanged(spellClass)
+                                expanded = false
+                            }
+                        )
+                    }
                 }
             }
         }
