e1bd15c5f0a07557fa1c2cc2b1cd4c4e89e2bf9f
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-05-13 17:17:20 +0200

    mage the spell search list one of the most magical screens ever

--- app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/SpellCard.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/SpellCard.kt
@@ -1,28 +1,33 @@
 package com.github.arhor.spellbindr.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.StarOutline
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.github.arhor.spellbindr.data.model.Spell
 
 @Composable
 fun SpellCard(
     spell: Spell,
-    isFavorite: Boolean = false,
     onClick: () -> Unit = {},
-    onFavor: () -> Unit = {},
 ) {
     Row(
         modifier = Modifier
@@ -31,15 +36,19 @@ fun SpellCard(
             .padding(horizontal = 8.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        IconButton(
-            onClick = onFavor,
-            content = {
-                Icon(
-                    imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-                    contentDescription = if (isFavorite) "Remove from favorites" else "Add to favorites"
-                )
-            }
-        )
+        Box(
+            modifier = Modifier
+                .minimumInteractiveComponentSize()
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(color = Color.Transparent),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = null
+            )
+        }
         Text(
             text = spell.name,
             style = MaterialTheme.typography.bodyMedium,
