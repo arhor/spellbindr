758877532348818e33b11e9ededadc49da9479fe
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-05-25 06:58:41 +0200

    refactoring

--- app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/characters/creation/CharacterCreationViewModel.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/characters/creation/CharacterCreationViewModel.kt
@@ -1,13 +1,10 @@
 package com.github.arhor.spellbindr.ui.screens.characters.creation
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.github.arhor.spellbindr.data.model.Race
 import com.github.arhor.spellbindr.data.repository.RaceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,33 +12,10 @@ class CharacterCreationViewModel @Inject constructor(
     private val raceRepository: RaceRepository
 ) : ViewModel() {
 
-    private val _races = MutableStateFlow<List<Race>>(emptyList())
-    val races: StateFlow<List<Race>> = _races
+    data class State(
+        val name: String = "",
+    )
 
-    private val _selectedRace = MutableStateFlow<Race?>(null)
-    val selectedRace: StateFlow<Race?> = _selectedRace
-
-    private val _currentStep = MutableStateFlow(0)
-    val currentStep: StateFlow<Int> = _currentStep
-
-    private val _characterName = MutableStateFlow("")
-    val characterName: StateFlow<String> = _characterName
-
-    init {
-        viewModelScope.launch {
-            _races.value = raceRepository.findRaces()
-        }
-    }
-
-    fun selectRace(race: Race) {
-        _selectedRace.value = race
-    }
-
-    fun goToStep(step: Int) {
-        _currentStep.value = step
-    }
-
-    fun setCharacterName(name: String) {
-        _characterName.value = name
-    }
+    private val _state = MutableStateFlow(State())
+    val state = _state.asStateFlow()
 }
