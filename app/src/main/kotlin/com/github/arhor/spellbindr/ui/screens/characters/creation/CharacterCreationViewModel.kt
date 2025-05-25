6f4856a5bdddfad4ed41e43b9ec857daf77cf7ec
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-05-25 07:12:11 +0200

    draft of the NameAndBackgroundScreen

--- app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/characters/creation/CharacterCreationViewModel.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/ui/screens/characters/creation/CharacterCreationViewModel.kt
@@ -5,6 +5,7 @@ import com.github.arhor.spellbindr.data.repository.RaceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,8 +15,27 @@ class CharacterCreationViewModel @Inject constructor(
 
     data class State(
         val name: String = "",
+        val background: String = "",
+        val alignment: String? = null,
+        val backstory: String? = null,
     )
 
     private val _state = MutableStateFlow(State())
     val state = _state.asStateFlow()
+
+    fun onNameChanged(newName: String) {
+        _state.update { it.copy(name = newName) }
+    }
+
+    fun onBackgroundChanged(newBackground: String) {
+        _state.update { it.copy(background = newBackground) }
+    }
+
+    fun onAlignmentChanged(newAlignment: String?) {
+        _state.update { it.copy(alignment = newAlignment) }
+    }
+
+    fun onBackstoryChanged(newBackstory: String?) {
+        _state.update { it.copy(backstory = newBackstory) }
+    }
 }
