0ca08960da2b1b1ea83a178e6110591646e51230
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-05-31 19:39:13 +0200

    refactoring

--- app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Option.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Option.kt
@@ -5,27 +5,17 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class Option {
-    abstract val optionType: String
-
-    @Serializable
-    open class BaseOption(
-        @SerialName("option_type")
-        override val optionType: String
-    ) : Option()
 
     @Serializable
     @SerialName("reference")
     data class ReferenceOption(
-        @SerialName("option_type")
-        override val optionType: String = "reference",
+
         val item: EntityRef
     ) : Option()
 
     @Serializable
     @SerialName("action")
     data class ActionOption(
-        @SerialName("option_type")
-        override val optionType: String = "action",
         @SerialName("action_name")
         val actionName: String,
         val count: Int? = null,
@@ -36,24 +26,18 @@ sealed class Option {
     @Serializable
     @SerialName("multiple")
     data class MultipleOption(
-        @SerialName("option_type")
-        override val optionType: String = "multiple",
         val items: List<Option>
     ) : Option()
 
     @Serializable
     @SerialName("string")
     data class StringOption(
-        @SerialName("option_type")
-        override val optionType: String = "string",
         val string: String
     ) : Option()
 
     @Serializable
     @SerialName("ideal")
     data class IdealOption(
-        @SerialName("option_type")
-        override val optionType: String = "ideal",
         val desc: String,
         val alignments: List<EntityRef>
     ) : Option()
@@ -61,8 +45,6 @@ sealed class Option {
     @Serializable
     @SerialName("counted_reference")
     data class CountedReferenceOption(
-        @SerialName("option_type")
-        override val optionType: String = "counted_reference",
         val count: Int,
         val of: EntityRef,
         val prerequisites: List<CountedReferencePrerequisite>? = null
@@ -71,8 +53,6 @@ sealed class Option {
     @Serializable
     @SerialName("score_prerequisite")
     data class ScorePrerequisiteOption(
-        @SerialName("option_type")
-        override val optionType: String = "score_prerequisite",
         val abilityScore: EntityRef,
         val minimumScore: Int
     ) : Option()
@@ -80,8 +60,6 @@ sealed class Option {
     @Serializable
     @SerialName("ability_bonus")
     data class AbilityBonusOption(
-        @SerialName("option_type")
-        override val optionType: String = "ability_bonus",
         val abilityScore: EntityRef,
         val bonus: Int
     ) : Option()
@@ -89,8 +67,6 @@ sealed class Option {
     @Serializable
     @SerialName("breath")
     data class BreathOption(
-        @SerialName("option_type")
-        override val optionType: String = "breath",
         val name: String,
         val dc: DifficultyClass,
         val damage: List<Damage>? = null
@@ -99,8 +75,6 @@ sealed class Option {
     @Serializable
     @SerialName("damage")
     data class DamageOption(
-        @SerialName("option_type")
-        override val optionType: String = "damage",
         val damageType: EntityRef,
         val damageDice: String,
         val notes: String? = null
@@ -109,8 +83,6 @@ sealed class Option {
     @Serializable
     @SerialName("choice")
     data class ChoiceOption(
-        @SerialName("option_type")
-        override val optionType: String = "choice",
         val choice: Choice
     ) : Option()
 }
