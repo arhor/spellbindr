e381e47fdd588809acbcabd32c5db1144dc1d89d
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-05-31 19:42:46 +0200

    refactoring

--- app/src/main/kotlin/com/github/arhor/spellbindr/data/model/ActionItem.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/data/model/ActionItem.kt
@@ -1,11 +1,9 @@
 package com.github.arhor.spellbindr.data.model
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ActionItem(
-    @SerialName("action_name")
     val actionName: String,
     val count: String,
     val type: String
