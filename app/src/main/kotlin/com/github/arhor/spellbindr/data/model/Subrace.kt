11e4ec08a6ccb1fc0e6b413db372fa22ad0520cd
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-06-09 12:46:54 +0200

    docs: add kdoc to Race and Subrace data classes
    Adds KDoc documentation to the `Race` and `Subrace` data classes, describing their properties.

--- app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Subrace.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Subrace.kt
@@ -2,6 +2,13 @@ package com.github.arhor.spellbindr.data.model
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a subrace with a name, description, and a list of traits.
+ *
+ * @property name The name of the subrace.
+ * @property desc A description of the subrace.
+ * @property traits A list of trait IDs associated with the subrace.
+ */
 @Serializable
 data class Subrace(
     val name: String,
