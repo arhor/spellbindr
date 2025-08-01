50495119cfb3547d4941bc1b4673613449f6f441
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-08-01 17:34:54 +0200

    feat: Enhance equipment choice options in data model
    This commit introduces a new `EquipmentCategoriesChoice` data class to represent equipment choices limited to specific categories. The existing `EquipmentChoice` class has been updated to reflect a change in its type. Additionally, detailed documentation has been added to clarify the purpose and properties of the new and existing classes.
    
    Key changes:
    - Added `EquipmentCategoriesChoice` for category-specific equipment selection.
    - Updated `EquipmentChoice` type to `equipment-categories`.
    - Enhanced documentation for clarity on choice options.

--- app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Choice.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Choice.kt
@@ -67,15 +67,53 @@ sealed interface Choice {
         )
     }
 
+    /**
+     * Represents a set of equipment options that a character can choose from.
+     *
+     * @property choose The number of equipment items the character can choose.
+     * @property from A list of strings representing the IDs of the available equipment items.
+     */
     @Serializable
     @SerialName("equipment")
     data class EquipmentChoice(
         override val choose: Int,
+        val from: List<String>,
+    ) : Choice
+
+    /**
+     * Represents a set of options where the character can choose equipment from specific categories.
+     * This is used when the choices are limited to certain types of equipment.
+     *
+     * @property choose The number of equipment items the character can choose from the specified categories.
+     * @property from An [EquipmentOption] object that defines the categories of equipment to choose from.
+     */
+    @Serializable
+    @SerialName("equipment-categories")
+    data class EquipmentCategoriesChoice(
+        override val choose: Int,
         val from: EquipmentOption,
     ) : Choice {
+        /**
+         * Represents an option for choosing equipment based on categories.
+         *
+         * @property categories A list of strings representing the equipment categories to choose from.
+         */
         @Serializable
         data class EquipmentOption(
             val categories: List<String>,
         )
     }
+
+    /**
+     * Represents a set of options where the character can choose from a list of other choice sets.
+     * This allows for complex, multi-level choices.
+     *
+     * @property from A list of [Choice] objects representing the nested choice sets.
+     */
+    @Serializable
+    @SerialName("nested-choice")
+    data class NestedChoice(
+        override val choose: Int,
+        val from: List<Choice>,
+    ): Choice
 }
