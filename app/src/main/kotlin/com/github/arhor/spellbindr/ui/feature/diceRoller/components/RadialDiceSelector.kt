343314cc5f411f7f8b990b369d2a8d74b7064729
Maksim Buryshynets <maksim.buryshynets@noom.com>
2025-10-20 17:46:43 +0200

    refactor: Move RadialDiceSelector to components package
    This commit moves the `RadialDiceSelector.kt` file into a new `components` subpackage within the `diceRoller` feature directory. The package declaration inside the file has been updated accordingly to reflect this structural change. This improves project organization by grouping component files together.

--- app/src/main/kotlin/com/github/arhor/spellbindr/ui/feature/diceRoller/components/RadialDiceSelector.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/ui/feature/diceRoller/components/RadialDiceSelector.kt
@@ -1,4 +1,4 @@
-package com.github.arhor.spellbindr.ui.feature.diceRoller
+package com.github.arhor.spellbindr.ui.feature.diceRoller.components
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
