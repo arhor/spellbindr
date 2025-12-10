6fc0ed5742dc5fcd0f25bdcd252f31d5d433ba9d
Maksim Buryshynets <38556161+arhor@users.noreply.github.com>
2025-12-10 14:10:42 +0100

    chore: enrich feats data with effects

--- app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Feat.kt
+++ app/src/main/kotlin/com/github/arhor/spellbindr/data/model/Feat.kt
@@ -7,5 +7,10 @@ data class Feat(
     val id: String,
     val name: String,
     val desc: List<String>,
-    val prerequisites: List<FeatPrerequisite>
+    val prerequisites: List<Prerequisite> = emptyList(),
+    val effects: List<Effect>? = null,
+    val abilityBonusChoice: Choice? = null,
+    val spellChoice: Choice? = null,
+    val languageChoice: Choice? = null,
+    val proficiencyChoice: Choice? = null,
 )
