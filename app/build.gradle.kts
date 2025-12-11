126043c1eba204505d068d52d783508eb74ee41f
Maksim Buryshynets <38556161+arhor@users.noreply.github.com>
2025-12-11 08:41:22 +0100

    chore: catalog junit5 plugin (#57)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.android.junit5)
 }
 
 android {
