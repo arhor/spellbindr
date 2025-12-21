b16eb114e8ace66426869485501d01af2fa35fe1
Maksim Buryshynets <38556161+arhor@users.noreply.github.com>
2025-12-21 16:20:11 +0100

    Add reference data use cases and tests (#90)

---
+++ app/src/main/kotlin/com/github/arhor/spellbindr/domain/usecase/ObserveAlignmentsUseCase.kt
@@ -0,0 +1,12 @@
+package com.github.arhor.spellbindr.domain.usecase
+
+import com.github.arhor.spellbindr.domain.model.Alignment
+import com.github.arhor.spellbindr.domain.repository.AlignmentRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObserveAlignmentsUseCase @Inject constructor(
+    private val alignmentRepository: AlignmentRepository,
+) {
+    operator fun invoke(): Flow<List<Alignment>> = alignmentRepository.allAlignments
+}
