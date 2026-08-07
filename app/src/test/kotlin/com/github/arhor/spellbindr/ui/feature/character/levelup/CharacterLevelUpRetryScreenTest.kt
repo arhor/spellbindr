0d9b59c362a7a284046f45b23f726b22fe40e465
Maksim Buryshynets <38556161+arhor@users.noreply.github.com>
2026-08-07 19:59:44 +0200

    test: cover level-up retry action

---
+++ app/src/test/kotlin/com/github/arhor/spellbindr/ui/feature/character/levelup/CharacterLevelUpRetryScreenTest.kt
@@ -0,0 +1,59 @@
+package com.github.arhor.spellbindr.ui.feature.character.levelup
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertDoesNotExist
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.arhor.spellbindr.ui.theme.AppTheme
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CharacterLevelUpRetryScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun `retryable failure should show retry and dispatch retry intent`() {
+        // Given
+        val intents = mutableListOf<CharacterLevelUpIntent>()
+        composeTestRule.setContent {
+            AppTheme {
+                CharacterLevelUpRouteContent(
+                    state = CharacterLevelUpUiState.Failure(
+                        message = "Unable to load level-up reference data",
+                        canRetry = true,
+                    ),
+                    dispatch = intents::add,
+                )
+            }
+        }
+
+        // When
+        composeTestRule.onNodeWithText("Retry").performClick()
+
+        // Then
+        assertThat(intents).containsExactly(CharacterLevelUpIntent.RetryClicked)
+    }
+
+    @Test
+    fun `terminal failure should not show retry`() {
+        // Given
+        composeTestRule.setContent {
+            AppTheme {
+                CharacterLevelUpRouteContent(
+                    state = CharacterLevelUpUiState.Failure("Character not found"),
+                    dispatch = {},
+                )
+            }
+        }
+
+        // Then
+        composeTestRule.onNodeWithText("Retry").assertDoesNotExist()
+    }
+}
